fc199d4ba26f6a5c39ee4ad1cdb97186d453a7c7
Albert Duran <Albertdug@Alberts-MacBook-Pro.local>
2016-05-05 16:20:39 -0300

    Changing MessageService

--- src/main/java/org/albert/brains/messenger/service/MessageService.java
+++ src/main/java/org/albert/brains/messenger/service/MessageService.java
@@ -3,9 +3,12 @@ package org.albert.brains.messenger.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.albert.brains.messenger.database.DataBase;
 import org.albert.brains.messenger.model.Message;
 
 public class MessageService {
+	
+	private Map<Long, Message> messages = DataBase.getMessages();
 	
 	public List<Message> getAllMessages() {
 		Message m1 = new Message(1L, "Hola mundo", null, "Albert");
