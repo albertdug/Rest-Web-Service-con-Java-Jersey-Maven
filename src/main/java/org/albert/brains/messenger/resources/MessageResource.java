d7a17189df80c339a6ac0727305f4ee2da306dd5
Albert Duran <albertrdg@gmail.com>
2016-05-06 18:25:29 -0300

    Implementing Subresources

--- src/main/java/org/albert/brains/messenger/resources/MessageResource.java
+++ src/main/java/org/albert/brains/messenger/resources/MessageResource.java
@@ -61,4 +61,9 @@ public class MessageResource {
 		return messageService.getMessage(id);
 	}
 	
+	@Path("/{messageId}/comments")
+	public CommentResource getCommentResource() {
+		return new CommentResource();
+	}
+	
 }
