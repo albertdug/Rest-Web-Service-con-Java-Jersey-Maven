b04eb14f58a8afcc391c6a94deef711f0aa65481
Albert Duran <Albertdug@Alberts-MacBook-Pro.local>
2016-05-06 09:49:53 -0300

    Implementing POST Method

--- src/main/java/org/albert/brains/messenger/resources/MessageResource.java
+++ src/main/java/org/albert/brains/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.albert.brains.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,6 +23,13 @@ public class MessageResource {
 	public List<Message> getMessage() { 
 		return messageService.getAllMessages();
 	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) { 
+		return messageService.addMessage(message);
+	} 
 	
 	@GET
 	@Path("/{messageId}")
